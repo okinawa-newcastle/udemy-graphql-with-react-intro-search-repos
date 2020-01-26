0db30acf474ea9e8e847f4ad89ec42ea0bf38d7a
ryutetsu shinjo <>
2020-01-26 18:46:16 +0900

    setup dotenv

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react'
 
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+console.log(GITHUB_TOKEN)
 
-function App() {
-  return (
-    <div >
-      Hello graphql
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>Hello, GraphQL</div>
+    )
+  }
 }
 
-export default App;
+export default App
+
